e5230d188fe3107dd084c4a63776925fdcd94321
Arulpriya <32356523+Arulpriya@users.noreply.github.com>
2020-07-22 07:55:34 +0530

    Added sample to add image in RowHeader cell

---
+++ Converter/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SfDataGridDemo
+{
+    public class RowHeaderConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //var orderInfo = value as OrderInfo;
+            if ((bool)value)
+            {
+                return "Images/no.png";
+               // return "Images/yes.png";
+            }
+            else
+            {
+                //return "Images/yes.png";
+                return null;
+            }           
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Cells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+//using Syncfusion.UI.Xaml.Grid.Helpers;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using Syncfusion.Data;
+using Syncfusion.Data.Extensions;
+using System.ComponentModel;
+using System.Collections;
+using System.Globalization;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Lockedbutton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var record in datagrid.View.Records)
+            {
+                var data = (record as RecordEntry).Data as OrderInfo;
+                if (data.OrderID % 3 == 0)
+                {
+                    //To change the image at run time.
+                    data.IsLocked = true;
+                }
+            }
+        }
+    }
+}   
+
